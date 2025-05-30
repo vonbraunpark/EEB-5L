794565c98f81da577683af72993732ae813e5d83
puppy1012 <puppy1012@naver.com>
2025-05-30 12:17:00 +0900

    feat: [이승현] 이미지출력연습

--- frontend/SeungheyonLee/html-container/src/App.tsx
+++ frontend/SeungheyonLee/html-container/src/App.tsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import { CircularProgress } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+//"htmlCssTestApp"이라는 이름으로 등록된 앱에서 "App"이라는 컴포넌트를 불러오겠다"
+// 이건 그냥 로컬에서 import 하는 게 아니라 "다른 앱(html-css-test-app)이 외부로 제공한 컴포넌트를 동적으로 불러오는" 거야.
 const HtmlCssTestApp = lazy(() => import("htmlCssTestApp/App"));
 
 const App = () => {
--- frontend/SeungheyonLee/html-css-test-app/src/App.tsx
+++ frontend/SeungheyonLee/html-css-test-app/src/App.tsx
@@ -1,6 +1,4 @@
 // React 18 방식으로 DOM에 렌더링을 위한 모듈
-import ReactDOM from "react-dom/client";
-
 // CSS 파일 불러오기 (스타일 적용)
 import "./style/main.css";
 
@@ -30,6 +28,44 @@ const App = () => {
             >
                 HTML & CSS 학습 공간
             </Typography>
+
+
+            <h1> HTML & CSS 학습을 진행하겠다 </h1>
+            <p>이 단락은 HTML과 CSS에 대해 알아보는 단락이다.</p>
+
+            <div className="box">
+                <h2>HTML 구조</h2>
+                <p>HTML은 제목, 단락 목록 등으로 구성됨</p>
+                <ul>
+                    <li>&lt;h1&gt; ~ &lt;h6&gt;: 제목</li>
+                    <li>&lt;p&gt;: 단락</li>
+                    <li>&lt;a href="#"&gt;: 하이퍼링크</li>
+                    <li>&lt;img src="..."&gt;: 이미지</li>
+                    <li>&lt;table&gt;, &lt;ul&gt;, &lt;ol&gt;: 표, 목록 만들기</li>
+                </ul>
+            </div>
+            <div className="box">
+                <h2>CSS 기본 개념</h2>
+                <p>CSS는 HTML 요소를 스타일링 하기위한 목적으로 사용함</p>
+                <ul>
+                    <li>선택자 : 요소를 선택(예:</li>
+                    <li>속성: 스타일을 정의(예:color,font-size</li>
+                    <li>값: 특정 스타일을 지정(예:red,px)</li>
+                </ul>
+            </div>
+            <div className="box">
+                <h2>링크 및 내비게이션</h2>
+                <p><code>&lt;a&gt;</code>태그를 사용하여 이미지를 표시할 수 있음</p>
+                <a href="http://www.w3schools.com/html">
+                    html 더 살펴보기
+                </a>
+            </div>
+
+            <div className="box">
+                <h2>이미지 표현</h2>
+                <p><code>&lt;img&gt;</code> 태그를 사용하여 이미지를 표시할 수 있음</p>
+                <img src="https://picsum.photos/200" alt="예제"/>
+            </div>
         </div>
     );
 };
@@ -37,3 +73,5 @@ const App = () => {
 
 // 다른 모듈에서 App을 사용할 수 있도록 내보냄
 export default App;
+//스크넘?을 통해서 각자 담당할 내용을 정리,번호 부여해서 부여
+//이후 정상작동하는지 확인 후 git에 pr(pull request)올리기
