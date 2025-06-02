fd23d31df149f988286bf5969cde3a1a452b7f00
puppy1012 <puppy1012@naver.com>
2025-06-02 15:45:50 +0900

    feat: [이승현] ControlFlow [EEB-5L-1]

--- frontend/SeungheyonLee/javascript-test-app/src/App.tsx
+++ frontend/SeungheyonLee/javascript-test-app/src/App.tsx
@@ -1,14 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 import "./index.css";
+import {Container, Typography} from "@mui/material";
+import {CalculationExample} from "./math_operation/CalculationExample.tsx";
+import {VariableAndDataTypes} from "./data_type/VariableAndDataTypes.tsx";
+import {LogicalOperationExample} from "./Logical_operation/LogicalOperationExample.tsx";
+import {TypeCoercion} from "./type_coercion/TypeCoercion.tsx";
+import {ControlFlowIf} from "./control_flow/ControlFlowIf.tsx";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: javascript-test-app</div>
-    <div>Framework: react-18</div>
-  </div>
-);
+const App = () => {
+    return (
 
-const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
-root.render(<App />);
+        <Container maxWidth="md" sx={{padding: "32px 16px", textAlign: "left"}}>
+            <Typography variant="h4" gutterBottom sx={{textAlign: "center"}}>
+                JavaScript 필수 요소를 파악해보자!
+            </Typography>
+
+            <CalculationExample/>
+            <VariableAndDataTypes/>
+            <LogicalOperationExample/>
+            <TypeCoercion/>
+            <ControlFlowIf/>
+        </Container>
+    )
+}
+export default App;
