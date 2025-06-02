b8481777a62b7f1057e19659e4d8db1bb3401bbf
Sanghoon Lee <gcccompil3r@gmail.com>
2025-06-02 14:22:58 +0900

    feat: [이상훈] 기본 연산 [EEB-5L-11]

--- frontend/notes/javascript-test-app/src/App.tsx
+++ frontend/notes/javascript-test-app/src/App.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import {Typography} from "@mui/material";
+import {CalculationExample} from "./math_operation/CalculationExample.tsx";
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
             <Typography variant="h4" gutterBottom>
                 JavaScript 필수 요소를 파악해보자!
             </Typography>
+            <CalculationExample/>
         </div>
     )
 }
