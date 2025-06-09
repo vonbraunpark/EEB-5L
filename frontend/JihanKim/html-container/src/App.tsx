e693d9f9b07ee7b65f368e19781f28834ca50d50
IsKJH <dkdlrh147@gmail.com>
2025-06-09 14:41:13 +0900

    feat [김지한] 학습 [EEB-5L-1]

--- frontend/JihanKim/html-container/src/App.tsx
+++ frontend/JihanKim/html-container/src/App.tsx
@@ -1,21 +1,31 @@
-import React, { lazy, Suspense } from "react";
+import React, {lazy, Suspense, useEffect, useState} from "react";
 import ReactDOM from "react-dom/client";
 
-import { CircularProgress } from "@mui/material";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {CircularProgress} from "@mui/material";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
+const NavigationBarApp = lazy(() => import("navigationBarApp/App"));
 const HtmlCssTestApp = lazy(() => import("htmlCssTestApp/App"));
 const JavascriptTestApp = lazy(() => import("javascriptTestApp/App"));
 
 const App = () => {
+    const [isNavigationBarLoaded, setIsNavigationBarLoaded] = useState(false);
+
+    useEffect(() => {
+        import("navigationBarApp/App")
+            .then(() => setIsNavigationBarLoaded(true))
+            .catch((err) => console.error("Failed to load navigation bar:", err));
+    }, []);
 
     return (
         <BrowserRouter>
-            <Suspense fallback={<CircularProgress />}>
+            <Suspense fallback={<CircularProgress/>}>
+                <NavigationBarApp/>
+
                 <Routes>
-                    <Route path="/" element={<div>Home Page</div>} />
-                    <Route path="/html-css-test" element={<HtmlCssTestApp />} />
-                    <Route path="/js-test" element={<JavascriptTestApp />} />
+                    <Route path="/" element={<div>Home Page</div>}/>
+                    <Route path="/html-css-test" element={<HtmlCssTestApp/>}/>
+                    <Route path="/js-test" element={<JavascriptTestApp/>}/>
                 </Routes>
             </Suspense>
         </BrowserRouter>
@@ -30,4 +40,4 @@ if (!container) {
 }
 
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(<App/>);
