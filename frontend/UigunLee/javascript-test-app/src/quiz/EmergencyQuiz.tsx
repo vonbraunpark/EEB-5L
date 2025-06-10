aa075b66383b9f2849339d9d13598b4e96f0c7c2
leeeeegun <leeeeegun@naver.com>
2025-06-10 11:42:19 +0900

    feat: [이의건] 수업 내용 저장 [EEB-5L-3]

---
+++ frontend/UigunLee/javascript-test-app/src/quiz/EmergencyQuiz.tsx
@@ -0,0 +1,35 @@
+export const EmergencyQuiz = () => {
+    let appleCount = 100
+    let applePrice = 10000
+    let userWannaBuyCount = 5
+
+    const LOOP_MAX = 3
+    const LOOP_MIN = 2
+
+    let queue = [];
+    let loopCount = Math.floor(Math.random() * (LOOP_MAX - LOOP_MIN + 1)) + LOOP_MIN
+
+    for (let i=0; i<loopCount; i++) {
+        const randomNumber = Math.floor(Math.random() * (LOOP_MAX - LOOP_MIN + 1)) + LOOP_MIN
+        queue.push(randomNumber)
+    }
+
+    return (
+        <div className="bg-gray-100 p-5 my-4 rounded-md border-2 border-blue-500 font-bold text-left">
+            <p>현재 사과의 개수 {appleCount}</p>
+            <p>현재 사과의 가격: {applePrice}</p>
+            <p>구매자가 원하는 사과 개수: {userWannaBuyCount}</p>
+            <p>랜덤 루프 몇 개냐? {loopCount}</p>
+            <p>queue: {queue.join(', ')}</p>
+
+            <pre>
+                {`
+                    1. 과일 가게 사장
+                    2. 과일을 온라인으로 판매하려 하는데 어떻게 해야 할까요?
+                    
+                    3. Queue를 시뮬레이션 하기
+                `}
+            </pre>
+        </div>
+    )
+}
