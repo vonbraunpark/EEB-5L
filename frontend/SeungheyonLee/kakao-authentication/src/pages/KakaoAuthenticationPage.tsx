98b1d0ae0d7643c79c57e80ffd1bd195230f863c
puppy1012 <puppy1012@naver.com>
2025-06-11 17:39:19 +0900

    feat: [이승현] module-federation 통일화 [EEB-5L-9]

--- frontend/SeungheyonLee/kakao-authentication/src/pages/KakaoAuthenticationPage.tsx
+++ frontend/SeungheyonLee/kakao-authentication/src/pages/KakaoAuthenticationPage.tsx
@@ -6,10 +6,11 @@ import {useNavigate} from "react-router-dom";
 import env from "../env.ts";
 
 const KakaoAuthenticationPage: React.FC = () => {
+    //우리가 Routes로 감싸놓은 Router 설정을 활용 할 수 있게 지원함
     const navigate = useNavigate();
-
     const handleKakaoLogin = () => {
         const kakaoAuthenticationUrl = env.api.KAKAO_AUTHENTICATION_URL;
+        // 팝업 형식의 로그인 창을 띄운다.
         const popup = window.open(kakaoAuthenticationUrl, '_blank', 'width=500,height=600');
 
         if (!popup) {
@@ -18,6 +19,7 @@ const KakaoAuthenticationPage: React.FC = () => {
         }
 
         const receiveMessage = (event: MessageEvent) => {
+            //팝업으로 메시지가 전달되면 receiveMessage() 니가 받아라
             console.log('📨 받은 메시지:', event.origin, event.data);
 
             // origin 검사 완화
@@ -31,7 +33,9 @@ const KakaoAuthenticationPage: React.FC = () => {
                 console.warn('❌ accessToken 없음');
                 return;
             }
-
+            //f12 눌렀을때 Application 파트에 localStorage가 보인다
+            // 해당 파트에 보면 userToken이 생성되어 있는 것을 볼 수 있는데
+            // 아래 파트에서 작업하는 것이라 보면 되겠다.
             localStorage.setItem('userToken', accessToken);
             window.dispatchEvent(new Event("user-token-changed"));
 
@@ -52,12 +56,30 @@ const KakaoAuthenticationPage: React.FC = () => {
 
         window.addEventListener('message', receiveMessage);
     };
-
+    //항상 컴포넌트의 returndp표현되는것이 UI에 해당한다.
     return (
+        //Boxsms React Mui(Vue Vuetify)에 해당하는 UI 템플릿이다
+        //<div> 태그에 해당하며 스타일을 조금 더 편리하게 지정할 수있도록 도와준다
+        //p: 4<- 내부 여백(padding)을 1당 8px로 4이므로 32px
+        //maxWidth:400 <- 최대 너비는 400px로 고정
+        //margin: "0 auto"<-가운데 정렬
         <Box sx={{ p: 4, maxWidth: 400, margin: "0 auto" }}>
+            {/*
+                TypoGraphy는 텍스트 표현 표현
+                variant="h5" HTML으 <h5> 처럼 스타일링한다.
+                gutterBottom :=<아래쪽에 적절한 여백을 만들어준다.
+            */}
             <Typography variant="h5" gutterBottom>
                 로그인
             </Typography>
+            {/*
+                SocialloginButton은 사실상 또다른 컴포넌트
+                컴포넌트 내부에서 또다른 컴포넌트를 사용 할 수 있다.
+                provider는 실제 SocialLoginButton이 사용하는 파라미터
+                provider를 사용하는 이유는 "kakao" 로그인임을 명시하기 위함이다
+                onClick도 SocialLogicButton이 사용하는 파라미터
+                onClick의 경우 클릭 했을때 무엇을 할것인지 외부에서 주입하기 위함이다.
+            */}
             <SocialLoginButton provider="kakao" onClick={handleKakaoLogin} />
         </Box>
     );
