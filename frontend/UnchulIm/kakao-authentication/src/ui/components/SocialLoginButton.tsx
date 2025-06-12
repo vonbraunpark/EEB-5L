02ff572e4c22370a13d0a538e12396aecdb746b6
GoldenDuck <unchul87@gmail.com>
2025-06-12 09:05:55 +0900

    feat:[임운철]카카오로그인 및 연습

--- frontend/UnchulIm/kakao-authentication/src/ui/components/SocialLoginButton.tsx
+++ frontend/UnchulIm/kakao-authentication/src/ui/components/SocialLoginButton.tsx
@@ -3,11 +3,15 @@ import KakaoLoginImage from "../../assets/kakao_login.png";
 
 type Provider = "google" | "kakao" | "github";
 
+//속성들을 props형태로 props라고 표현한다고 했는데
+//실제로 socialLoginButton컴포넌트가 사용하는 파라미터를 props형태로 표현하기 위해 작성
 interface Props {
     provider: Provider;
+    //입력이 없고 리턴도 없는 함수가 onClick입니다 라는 뜻
+    //void (*)(void);
     onClick: () => void;
 }
-
+//위의 Provider에서 지정한 kakao,google,gothub 등이 오면 아래의 정해진 lavel, color, icon값을 가지게 만든다라는 뜻
 const providerInfo: Record<Provider, {
     label: string;
     color: string;
@@ -15,8 +19,9 @@ const providerInfo: Record<Provider, {
 }> = {
     kakao: { label: "Kakao 로그인", color: "", icon: null },   // 사용하지 않음
 };
-
+//파라미터 2개 (provider 와 onClick함수)를 받아옴
 const SocialLoginButton: React.FC<Props> = ({ provider, onClick }) => {
+    // 위쪽의 lavel,color,icon정보를 획득함 -> 현재 provider는 kakao입니다
     const info = providerInfo[provider];
 
     if (!info.label) return null; // 유효하지 않은 provider 방어 처리
