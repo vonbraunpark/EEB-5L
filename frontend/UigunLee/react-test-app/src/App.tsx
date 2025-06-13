8871e1106ca1ee40e5ffc29dc3e31b9a740d1791
leeeeegun <leeeeegun@naver.com>
2025-06-13 11:03:02 +0900

    feat: [이의건] redux 기반 counter [EEB-5L-7]

--- frontend/UigunLee/react-test-app/src/App.tsx
+++ frontend/UigunLee/react-test-app/src/App.tsx
@@ -5,15 +5,21 @@ import "./index.css";
 import HelloPropsdTest from "./basics/HelloPropsdTest.tsx";
 import SimpleCounter from "./basics/SimpleCounter.tsx";
 import TodoListTest from "./basics/TodoListTest.tsx";
+import ReduxCounterTest from "./basics/ReduxCounterTest.tsx";
+import {Provider} from "react-redux";
+import {store} from "./state/stort.ts";
 
 const App = () => (
+    <Provider  store={store}>
     <div className="mt-10 text-3xl mx-auto max-w-6xl">
         <HelloPropsdTest/>
         <div>Name: navigation-bar-app</div>
         <div>Framework: react-18</div>
         <SimpleCounter/>
         <TodoListTest/>
+        <ReduxCounterTest/>
     </div>
+    </Provider>
 );
 
 export default App
