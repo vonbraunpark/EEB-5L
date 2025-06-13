c49a3ab34e7d6fe7e4968cfbdcd45fb7268d3ae3
HIhyen00 <99hello.0.k@gmail.com>
2025-06-13 12:06:24 +0900

    feat: [김혜인] 구글 인증 [EEB-5L]

--- frontend/HyeinKim/react-test-app/src/App.tsx
+++ frontend/HyeinKim/react-test-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {ReactNode} from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
@@ -6,16 +6,22 @@ import HelloPropsTest from "./basics/HelloPropsTest.tsx";
 import SimpleCounter from "./basics/SimpleCounter.tsx";
 import ConditionalRenderTest from "./basics/ConditionalRenderTest.tsx";
 import ToDoListTest from "./basics/ToDoListTest.tsx";
+import ReduxCounterTest from "./basics/ReduxCounterTest.tsx";
+import {store} from "./state/store.ts";
+import { Provider } from "react-redux";
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <HelloPropsTest/>
-    <div>Name: react-test-app</div>
-    <div>Framework: react-18</div>
-    <SimpleCounter/>
-    <ConditionalRenderTest/>
-    <ToDoListTest/>
-  </div>
+    <Provider store={store}>
+        <div className="mt-10 text-3xl mx-auto max-w-6xl">
+            <HelloPropsTest/>
+            <div>Name: react-test-app</div>
+            <div>Framework: react-18</div>
+            <SimpleCounter/>
+            <ConditionalRenderTest/>
+            <ToDoListTest/>
+            <ReduxCounterTest/>
+        </div>
+    </Provider>
 );
 
 export default App // 외부에서 받을 수 있게 함
