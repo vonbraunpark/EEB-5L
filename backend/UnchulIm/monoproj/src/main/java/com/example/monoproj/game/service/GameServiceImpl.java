d0767e26eb048c08e2ae9e618b6ec12a7abf48f3
GoldenDuck <unchul87@gmail.com>
2025-06-24 15:02:11 +0900

    feat:[임운철]test gameservice

---
+++ backend/UnchulIm/monoproj/src/main/java/com/example/monoproj/game/service/GameServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.monoproj.game.service;
+
+import com.example.monoproj.game.entity.Game;
+import com.example.monoproj.game.repository.GameRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GameServiceImpl implements GameService {
+    private final GameRepository gameRepository;
+
+    @Override
+    public Long start() {
+        Game game = new Game();
+        System.out.println("game: " + game);
+        Game savedGame = gameRepository.save(game);
+        System.out.println("savedGame: " + savedGame);
+        return savedGame.getId();
+    }
+}
+//do u know 발키뤼? 잇츠 굿?뽈유
