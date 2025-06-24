4c95fd0e1bb32d6464945cfded2c905c62a77aaa
redEevee <aaaa182118@gmail.com>
2025-06-24 17:11:07 +0900

    feat [이신원] 테스트 케이스 작성[EEB-5L-4]

---
+++ backend/SinwonLee/monoproj/src/main/java/com/example/monoproj/game/service/GameServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.monoproj.game.service;
+
+import com.example.monoproj.game.entity.Game;
+import com.example.monoproj.game.repository.GameRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GameServiceImpl implements GameService {
+    private final GameRepository gameRepository;
+
+    @Override
+    public Long start() {
+        Game game = new Game();
+        Game savedGame = gameRepository.save(game);
+        return savedGame.getId();
+    }
+}
