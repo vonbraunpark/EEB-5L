2c91e382bf68ef4ec2baadfba9330798073c68d0
Roto90 <slalala90@gmail.com>
2025-06-27 15:50:56 +0900

    remove [박태준] frontend 연습용 에셋 삭제 [EEB-5L-7]
    remove [박태준] frontend 연습용 에셋 삭제 [EEB-5L-7]

--- frontend/TeajunPark/vuetify-tailwind-board-app/src/plugins/webfontloader.ts
+++
@@ -1,8 +0,0 @@
-export async function loadFonts() {
-    const WebFont = await import('webfontloader')
-    WebFont.load({
-        google: {
-            families: ['Roboto:100,300,400,500,700,900'],
-        },
-    })
-}
