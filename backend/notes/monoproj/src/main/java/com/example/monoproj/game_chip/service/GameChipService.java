ca0b817c061ce2503aa72dc51ca2077d82aaab50
Sanghoon Lee <gcccompil3r@gmail.com>
2025-07-11 12:47:29 +0900

    feat: 인증된 사용자가 게임 칩 등록 [EEB-5L-71]

--- backend/notes/monoproj/src/main/java/com/example/monoproj/game_chip/service/GameChipService.java
+++ backend/notes/monoproj/src/main/java/com/example/monoproj/game_chip/service/GameChipService.java
@@ -1,10 +1,11 @@
 package com.example.monoproj.game_chip.service;
 
-import com.example.monoproj.game_chip.controller.request_form.RegisterGameChipRequestForm;
 import com.example.monoproj.game_chip.controller.response_form.ListGameChipResponseForm;
 import com.example.monoproj.game_chip.controller.response_form.RegisterGameChipResponseForm;
+import com.example.monoproj.game_chip.service.request.RegisterGameChipRequest;
+import com.example.monoproj.game_chip.service.response.RegisterGameChipResponse;
 
 public interface GameChipService {
-    RegisterGameChipResponseForm createGameChip(RegisterGameChipRequestForm requestForm);
+    RegisterGameChipResponse createGameChip(RegisterGameChipRequest request);
     ListGameChipResponseForm getAllGameChips();
 }
